9782fcf5c51cae3b14f54296c233e92492d15673
Michael Lu <yeskky@gmail.com>
2020-06-26 16:51:44 +0800

    first commit

---
+++ app/src/main/java/com/example/uilayouttest/Fruit.kt
@@ -0,0 +1,4 @@
+package com.example.uilayouttest
+
+class Fruit(val name:String, val imageId: Int) {
+}
---
+++ app/src/main/java/com/example/uilayouttest/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.uilayouttest
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.uilayouttest.chat.ChatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        btnListView.setOnClickListener {
+            startActivity(Intent(this, ListViewActivity::class.java))
+        }
+
+        btnRecyclerView.setOnClickListener {
+            startActivity(Intent(this, RecyclerViewActivity::class.java))
+        }
+
+        btnChatActivity.setOnClickListener {
+            startActivity(Intent(this, ChatActivity::class.java))
+        }
+    }
+}
