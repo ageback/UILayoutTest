4f610823993d3badf2c54974c293ace806ce47c2
Michael Lu <yeskky@gmail.com>
2020-06-30 15:02:18 +0800

    用SharedPreferences实现记住密码功能

--- app/src/main/java/com/example/uilayouttest/login/LoginActivity.kt
+++ app/src/main/java/com/example/uilayouttest/login/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.uilayouttest.login
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,9 +11,21 @@ import com.example.uilayouttest.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
+    private val TAG_REMEMBER_PASS =  "remember_password"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val prefs = getPreferences(Context.MODE_PRIVATE)
+        val isRemember = prefs.getBoolean(TAG_REMEMBER_PASS, false)
+        if(isRemember)
+        {
+            val account = prefs.getString("account", "")
+            val password = prefs.getString("password", "")
+            accountEdit.setText(account)
+            passwordEdit.setText(password)
+            chkRemberPass.isChecked = true
+        }
 
         btnLogin.setOnClickListener {
             val account = accountEdit.text.toString()
@@ -20,6 +33,16 @@ class LoginActivity : BaseActivity() {
 
             if(account=="lmh" && password == "1234")
             {
+                val editor = prefs.edit()
+                if(chkRemberPass.isChecked)
+                {
+                    editor.putBoolean(TAG_REMEMBER_PASS, true)
+                    editor.putString("account", account)
+                    editor.putString("password", password)
+                } else {
+                    editor.clear()
+                }
+                editor.apply()
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 finish()
