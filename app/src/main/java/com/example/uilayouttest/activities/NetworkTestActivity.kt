368e31becef2fc85229f951742ca119f18e26238
Michael Lu <yeskky@gmail.com>
2020-07-20 21:23:23 +0800

    解析XML和JSON数据

--- app/src/main/java/com/example/uilayouttest/activities/NetworkTestActivity.kt
+++ app/src/main/java/com/example/uilayouttest/activities/NetworkTestActivity.kt
@@ -2,12 +2,18 @@ package com.example.uilayouttest.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.uilayouttest.R
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_network_test.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.io.StringReader
 import java.net.HttpURLConnection
 import java.net.URL
 import kotlin.concurrent.thread
@@ -65,13 +71,14 @@ class NetworkTestActivity : AppCompatActivity() {
             try {
                 val client = OkHttpClient()
                 val request = Request.Builder()
-                    .url("https://www.baidu.com")
+                    .url("http://oa.wesoft.net.cn/get_data.json")
                     .build()
                 val response = client.newCall(request).execute()
                 val responseData = response.body?.string()
                 if(responseData!=null)
                 {
-                    showResponse(responseData)
+//                    showResponse(responseData)
+//                    parseXMLWithPull(responseData)
                 }
             } catch (e: Exception)
             {
@@ -79,4 +86,52 @@ class NetworkTestActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun parseJSONWithGSON(jsonData: String) {
+        val gson = Gson()
+        val typeOf = object : TypeToken<List<App>>() {}.type
+        val appList = gson.fromJson<List<App>>(jsonData, typeOf)
+        for (app in appList)
+        {
+            Log.d("NetworkTestActivity", "id is ${app.id}")
+            Log.d("NetworkTestActivity", "name is ${app.name}")
+            Log.d("NetworkTestActivity", "version is ${app.version}")
+        }
+    }
+
+    private fun parseXMLWithPull(xmlData: String) {
+        try {
+            val factory = XmlPullParserFactory.newInstance()
+            val xmlPullParser = factory.newPullParser()
+            xmlPullParser.setInput(StringReader(xmlData))
+            var eventType = xmlPullParser.eventType
+            var id=""
+            var name=""
+            var version=""
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                val nodeName = xmlPullParser.name
+                when (eventType) {
+                    XmlPullParser.START_TAG -> {
+                        when (nodeName) {
+                            "id" -> id = xmlPullParser.nextText()
+                            "name" -> name = xmlPullParser.nextText()
+                            "version" -> version = xmlPullParser.nextText()
+                        }
+                    }
+                    XmlPullParser.END_TAG -> {
+                        if("app" == nodeName) {
+                            Log.d("NetworkTestActivity", "id is $id")
+                            Log.d("NetworkTestActivity", "name is $name")
+                            Log.d("NetworkTestActivity", "version is $version")
+                        }
+                    }
+                }
+                eventType = xmlPullParser.next()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    class App(val id: String, val name: String, val version: String)
 }
